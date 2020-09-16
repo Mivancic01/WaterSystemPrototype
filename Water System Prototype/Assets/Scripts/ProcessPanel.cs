6e9d4ad9d10b5dd5762fa6685ad3180ba70866fb
Mivancic01 <m.ivancic@student.tugraz.at>
2020-09-16 19:38:24 +0200

    Progress bar scripts update

---
+++ Water System Prototype/Assets/Scripts/ProcessPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcessPanel : MonoBehaviour
+{
+    public ProgressBar progressBarObj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
