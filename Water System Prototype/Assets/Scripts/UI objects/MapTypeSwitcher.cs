6d6d1782ea86c469732a0c0fa71c78c6e554156c
Mivancic01 <m.ivancic@student.tugraz.at>
2020-10-05 08:16:07 +0200

    Minor UI rework and map switching

---
+++ Water System Prototype/Assets/Scripts/UI objects/MapTypeSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapTypeSwitcher : MonoBehaviour
+{
+    public Sprite simpleMap, sateliteMap, topographicMap;
+
+    public void SwitchMap(int type = 0)
+    {
+        var spriteComp = GetComponent<SpriteRenderer>();
+        switch(type)
+        {
+            case 0:
+                spriteComp.sprite = simpleMap;
+                break;
+            case 1:
+                spriteComp.sprite = sateliteMap;
+                break;
+            case 2:
+                spriteComp.sprite = topographicMap;
+                break;
+            default:
+                Debug.LogError("SETTING AN INVALID MAP!");
+                break;
+
+        }
+    }
+}
