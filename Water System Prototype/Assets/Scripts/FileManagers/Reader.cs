d24417bf14a271de100e31e456c78d3c3bf3bf51
Mivancic01 <m.ivancic@student.tugraz.at>
2020-10-16 11:46:37 +0200

    Updated reader to work with php

--- Water System Prototype/Assets/Scripts/FileManagers/Reader.cs
+++ Water System Prototype/Assets/Scripts/FileManagers/Reader.cs
@@ -8,24 +8,54 @@ public class Reader : MonoBehaviour
 {
     public GameObject loadGamePanel;
     public Lean.Gui.LeanButton buttonPrefab;
+    public bool isWebGlBuild = false;
+
+    public static string saveGameData = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        var fileEntries = Directory.GetFiles("SaveFiles/");
-        foreach (var file in fileEntries)
+        if (isWebGlBuild)
+            StartCoroutine(textLoad());
+        else
         {
-            if (!file.EndsWith(".txt"))
-                continue;
+            var fileEntries = Directory.GetFiles("SaveFiles/");
+            foreach (var file in fileEntries)
+            {
+                if (!file.EndsWith(".txt"))
+                    continue;
 
-            string saveGameName = file.Remove(file.LastIndexOf(".")).Substring(10);
+                string saveGameName = file.Remove(file.LastIndexOf(".")).Substring(10);
+
+                var btnObj = Instantiate(buttonPrefab, loadGamePanel.transform);
+                btnObj.GetComponentInChildren<Text>().text = saveGameName;
+
+
+                if (file.EndsWith(".txt"))
+                    Debug.Log("NEW FILE: " + file.Remove(file.LastIndexOf(".")).Substring(10));
+            }
+        }
+    }
+
+
+    IEnumerator textLoad()
+    {
+        bool successful = true;
+        WWWForm form = new WWWForm();
+        WWW www = new WWW("http://localhost:9000/tounity.php", form);
+        yield return www;
+
+        if (www.error != null)
+            successful = false;
+        else
+        {
+            saveGameData = www.text;
 
             var btnObj = Instantiate(buttonPrefab, loadGamePanel.transform);
-            btnObj.GetComponentInChildren<Text>().text = saveGameName;
+            btnObj.GetComponentInChildren<Text>().text = "Save1";
 
-
-            if (file.EndsWith(".txt"))
-                Debug.Log("NEW FILE: " + file.Remove(file.LastIndexOf(".")).Substring(10));
+            Debug.Log(www.text);
+            successful = true;
         }
     }
 }
